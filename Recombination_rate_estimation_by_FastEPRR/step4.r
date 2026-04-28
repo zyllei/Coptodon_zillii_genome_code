f5b1b5eb865481f24410151b3edbf188f94f1e60
zyllei <109718547+zyllei@users.noreply.github.com>
2026-04-28 15:13:53 +0800

    Update step4.r

--- Recombination_rate_estimation_by_FastEPRR/step4.r
+++ Recombination_rate_estimation_by_FastEPRR/step4.r
@@ -1,2 +1,2 @@
-library(FastEPRR)\
+library(FastEPRR)
 FastEPRR_rho2r(inputFilePath = "/dellfsqd2/ST_OCEAN/USER/zhangyaolei/Project/luofeiyu/review-comments2/ld-hat/fastEPRR/all.rho", outputFilePath = "/dellfsqd2/ST_OCEAN/USER/zhangyaolei/Project/luofeiyu/review-comments2/ld-hat/fastEPRR/Transform_Rho_cM/out", Ne = 18444)
