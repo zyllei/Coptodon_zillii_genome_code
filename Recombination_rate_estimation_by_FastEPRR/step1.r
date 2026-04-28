35c24453e14a8f1011fd3af58483e65873f66bc1
zyllei <109718547+zyllei@users.noreply.github.com>
2026-04-28 15:12:37 +0800

    Update step1.r

--- Recombination_rate_estimation_by_FastEPRR/step1.r
+++ Recombination_rate_estimation_by_FastEPRR/step1.r
@@ -1,10 +1,2 @@
-{\rtf1\ansi\ansicpg936\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww20440\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 library(FastEPRR)\
-FastEPRR_VCF_step1(vcfFilePath = "./chr1.vcf.bi-allelic.vcf.phase.gz", winLength = 10000, srcOutputFilePath= "fastEPRR/step1/chr1")\
-}
+library(FastEPRR)
+FastEPRR_VCF_step1(vcfFilePath = "./chr1.vcf.bi-allelic.vcf.phase.gz", winLength = 10000, srcOutputFilePath= "fastEPRR/step1/chr1")
