b388db045ab31fa88baa3b0f36cecd771b79d5a6
zyllei <109718547+zyllei@users.noreply.github.com>
2026-04-28 15:03:17 +0800

    Update gmap.sh

--- Genome_assembly_and_annotation/gmap.sh
+++ Genome_assembly_and_annotation/gmap.sh
@@ -5,4 +5,4 @@ cpu=10\
 output_format=gff3_gene\
 output_name=gmap.gff\
 \
-/dellfsqd2/ST_OCEAN/USER/lishuo1/01_software/gmap-2023-05-15/bin/gmap -D $reference -d $\{species\}_reference -t $cpu --suboptimal-score=0.9 -f $output_format $transcript > $output_name}
+/dellfsqd2/ST_OCEAN/USER/lishuo1/01_software/gmap-2023-05-15/bin/gmap -D $reference -d $\{species\}_reference -t $cpu --suboptimal-score=0.9 -f $output_format $transcript > $output_name
