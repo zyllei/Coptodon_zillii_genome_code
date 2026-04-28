e23ee84f78fc5343e73cd9b516e47a403d770eb8
zyllei <109718547+zyllei@users.noreply.github.com>
2026-04-28 15:09:52 +0800

    Update BUSTEDS.sh

--- Positive_selection_BUSTEDS-MH/BUSTEDS.sh
+++ Positive_selection_BUSTEDS-MH/BUSTEDS.sh
@@ -1,10 +1,3 @@
-{\rtf1\ansi\ansicpg936\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 source /dellfsqd2/C_OCEAN/USERS/wangchaoyi/myconda/etc/profile.d/conda.sh\
+source /dellfsqd2/C_OCEAN/USERS/wangchaoyi/myconda/etc/profile.d/conda.sh\
 conda activate hyphy_mamba\
-hyphy busted --alignment hhh.fasta --tree in.tree --srv Yes --multiple-hits Double+Triple --branches Foreground --output hhh_MH.json}
+hyphy busted --alignment hhh.fasta --tree in.tree --srv Yes --multiple-hits Double+Triple --branches Foreground --output hhh_MH.json
