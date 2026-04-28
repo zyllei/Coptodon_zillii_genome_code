8213e2fdec1ce6ff151e89201cc289b821c0f6f0
zyllei <109718547+zyllei@users.noreply.github.com>
2026-04-28 15:03:01 +0800

    Update merqury.sh

--- Genome_assembly_and_annotation/merqury.sh
+++ Genome_assembly_and_annotation/merqury.sh
@@ -3,4 +3,4 @@ mq="/dellfsqd2/ST_OCEAN/USER/wangjiahao/My_file/miniconda3/envs/merqury/share/me
 k=21\
 gzip -dc m64066_230102_000448.clean.fasta.gz m64067_221228_021855.clean.fasta.gz  > input.fa\
 meryl k=21 memory=50G threads=10 count output hifi.meryl input.fa\
-merqury.sh hifi.meryl genome.fa luofei.out}
+merqury.sh hifi.meryl genome.fa luofei.out
