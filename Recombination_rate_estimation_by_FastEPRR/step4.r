a73eeca192ace288653c1979ebe6a24888573228
zyllei <109718547+zyllei@users.noreply.github.com>
2026-04-28 15:13:41 +0800

    Update step4.r

--- Recombination_rate_estimation_by_FastEPRR/step4.r
+++ Recombination_rate_estimation_by_FastEPRR/step4.r
@@ -1,9 +1,2 @@
-{\rtf1\ansi\ansicpg936\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww20440\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 library(FastEPRR)\
-FastEPRR_rho2r(inputFilePath = "/dellfsqd2/ST_OCEAN/USER/zhangyaolei/Project/luofeiyu/review-comments2/ld-hat/fastEPRR/all.rho", outputFilePath = "/dellfsqd2/ST_OCEAN/USER/zhangyaolei/Project/luofeiyu/review-comments2/ld-hat/fastEPRR/Transform_Rho_cM/out", Ne = 18444)}
+library(FastEPRR)\
+FastEPRR_rho2r(inputFilePath = "/dellfsqd2/ST_OCEAN/USER/zhangyaolei/Project/luofeiyu/review-comments2/ld-hat/fastEPRR/all.rho", outputFilePath = "/dellfsqd2/ST_OCEAN/USER/zhangyaolei/Project/luofeiyu/review-comments2/ld-hat/fastEPRR/Transform_Rho_cM/out", Ne = 18444)
